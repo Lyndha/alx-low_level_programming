771238355207f9fb085048a6afac6fb62f07a1ae
linda <ihuomalyndha@gmail.com>
2023-04-20 06:37:42 -0700

    betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include stdio.h
+/**
+ * main - A C program that prints using puts function and not allowed to use printf.
+ * Return: 0 (this shows that my work is done correctly)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
