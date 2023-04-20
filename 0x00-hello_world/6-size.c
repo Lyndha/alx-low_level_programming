6466dc45c323bcc519182b9d3c2ec726aaafed9b
linda <ihuomalyndha@gmail.com>
2023-04-20 11:37:56 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long int d;
 	float f;
 
-printf("size of a char: % lu byte($)\n, (unasigned long)sizeof(a));
-printf("size of a int : % lu byte($)\n, (unasigned long)sizeof(b));
-printf("size of a long int: % lu byte($)\n, (unasigned long)sizeof(c));
-printf("size of a long long int: % lu byte($)\n, (unasigned long)sizeof(d));
-printf("size of a float: % lu byte($)\n, (unasigned long)sizeof(f));
+printf("size of a char: % lu byte($)\n, (unsigned long)sizeof(a));
+printf("size of a int : % lu byte($)\n, (unsigned long)sizeof(b));
+printf("size of a long int: % lu byte($)\n, (unsigned long)sizeof(c));
+printf("size of a long long int: % lu byte($)\n, (unsigned long)sizeof(d));
+printf("size of a float: % lu byte($)\n, (unsigned long)sizeof(f));
 return (0);
 }
