00bbce9f1b956769b3824467f92cc1e8e9ffec1a
linda <ihuomalyndha@gmail.com>
2023-04-20 06:57:23 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
  * main - A C program that prints using puts function and not allowed to use printf.
- * Return: 0 (this shows that my work is done correctly)
+ * Return: 0 (success)
  */
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
