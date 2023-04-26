0dab9b300773f1bef43c0fe11348f9d9a89236fe
linda <ihuomalyndha@gmail.com>
2023-04-26 10:15:49 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,23 +10,23 @@ void jack_bauer(void)
 	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 5; c++)
 			{
-				for (c = 0; c <= 5; c++)
+				for (d = 0; c <= 9; d++)
 				{
-					for (d = 0; c <= 9; d++)
-					{
-						if (a == 2 && b > 3)
-						{break;
-						}
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(':');
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
+					if (a == 2 && b > 3)
+					{break;
 					}
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(':');
+					_putchar(c + '0');
+					_putchar(d + '0');
+					_putchar('\n');
 				}
 			}
 		}
 	}
+}
 
