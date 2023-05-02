0fceb298ea1162f7b076a764fee7a720f6bb2042
linda <ihuomalyndha@gmail.com>
2023-05-02 08:14:55 -0700

    g

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: the sring to pring
+ * Return: void
+ */
+void print_rev(chat *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar ('\n')
+}
