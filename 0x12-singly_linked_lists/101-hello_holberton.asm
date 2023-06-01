ef2f135b1e10d37dab57926a2efb824066f94288
lyndha <ihuomalyndha@gmail.com>
2023-06-01 16:06:08 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,9 @@
-section .data
-	format db 'Hello, Holberton', 0
-
-section .text
-	extern printf
-
-global main
+global  main
+        extern    printf
 main:
-	mov edi, format
-	xor eax, eax
-	call printf
-	mov eax, 0
+	mov   edi, format
+	xor   eax, eax
+	call  printf
+	mov 	eax, 0
 	ret
+format: db `Hello, Holberton\n`,0
